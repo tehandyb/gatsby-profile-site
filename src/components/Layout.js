294de52e5410c0870d3a328fa49ed56bee487468
Andrew Blowe <tehandyb@gmail.com>
2019-03-16 18:53:05 +0000

    now using Helvetica-like font

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,23 +1,36 @@
 import React from 'react';
 import Helmet from 'react-helmet'
+import styled from '@emotion/styled'
+import { Global, css } from '@emotion/core'
 import '../../node_modules/normalize.css/normalize.css'
 import './Layout.scss'
+import TexGyreHelvetica from '../fonts/texgyreheros-regular-webfont.woff'
 
-const style = {
-  backgroundColor: 'black',
-  width: '100vw',
-  height: '100vh',
-  color: 'white',
-  fontFamily: "'Jura', sans-serif"
-};
+const globalStyles = css`
+  body {
+    background-color: black;
+  }
+  
+  @font-face {
+      font-family: 'TexGyreHelvetica';
+      src: url(${TexGyreHelvetica}) format('woff');
+      font-weight: normal;
+      font-style: normal;
+  }
+`;
+
+const Body = styled.div`
+  width: 100vw;
+  height: 100vh;
+  color: white;
+  font-family: 'TexGyreHelvetica';
+`;
 
 const Layout = ({ children }) => (
-  <div style={style}>
-    <Helmet>
-      <link href="https://fonts.googleapis.com/css?family=Jura" rel="stylesheet" />
-    </Helmet>
+  <Body>
+    <Global styles={globalStyles} />
     {children}
-  </div>
+  </Body>
 );
 
 export default Layout;
